83d194bd35e7d6a87cb3ef6a2f4bd5f763a543bf
James Horton <jameshorton@gmail.com>
2018-11-05 17:12:17 -0500

    completed bonus

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { Client } = require('pg');
+const {
+    Client
+} = require('pg');
 const express = require('express');
 
 // create an express application
@@ -16,24 +18,49 @@ app.get('/users', (req, res) => {
     });
 });
 
-app.post('/users',(req, res) => {
+app.post('/users', (req, res) => {
     const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
-    const values = [req.body.username,req.body.bio]
-    client.query(text,values, (err, result) => {
+    const values = [req.body.username, req.body.bio]
+    client.query(text, values, (err, result) => {
         const newUserID = result.rows[0].id
-        res.send({"newUserID":newUserID})
+        res.send({
+            "newUserID": newUserID
+        })
     })
 });
 
-app.get('/users/:id', (req, res) => {
-    // console.log(req.params.id)
-    const text = 'SELECT * FROM users WHERE id=$1';
+app.get('/users/:id', async function (req, res) {
+    const text = 'SELECT users.id AS uid, posts.*, users.bio, users.username FROM users FULL JOIN posts ON users.id=posts.user_id WHERE users.id=$1';
     const values = [req.params.id];
-    client.query(text,values, (err, result) => {
-        if(err) return res.status(500).send({error:err})
-        res.send(result.rows);
-    })
-});
+    try {
+        const result = await client.query(text, values)
+        console.log(result.rows)
+        let posts = result.rows[0].title === null?
+        []:
+        result.rows.map(result => {
+            let object = {
+                'id': result.id,
+                'title': result.title,
+                'body': result.body
+            }
+            return object
+        })
+        let body = {
+            "username": result.rows[0].username,
+            "bio": result.rows[0].bio,
+            // "id": values[0],
+            "id": result.rows[0].uid,
+            "posts":posts
+        }
+        res.send(body)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({"error:":err})
+    } 
+})
+
+
+
 
 
 
